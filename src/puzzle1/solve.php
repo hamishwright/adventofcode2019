b646c963111f55b1a8f01438d52ccba8cfcf0322
Hamish Wright <hamish.wright@mvfglobal.com>
2019-12-11 00:28:55 +0000

    revert to getting_started phase

--- src/puzzle1/solve.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-use PuzzleSolvers\Puzzle1\FuelCounterUpper;
-
-$fuelCounterUpper = new FuelCounterUpper('puzzle1/input.txt');
-$fuelCounterUpper->run();
-echo "answer: " . $fuelCounterUpper->getOutput() . "\n";
